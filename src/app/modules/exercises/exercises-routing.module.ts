6dd315eb241cc0403459ae7eb30687b17f695048
angedev07 <angelgaran20@gmail.com>
2023-06-07 19:15:42 -0500

    Creado routing para la lista de ejercicios y la tabla para visualizarlos

--- src/app/modules/exercises/exercises-routing.module.ts
+++ src/app/modules/exercises/exercises-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { EjerciciosComponent } from './pages/ejercicios/ejercicios.component';
 
 const routes: Routes = [
   {
     path: 'home', 
     component: HomeComponent
+  },
+  {
+    path: 'ejercicios',
+    component: EjerciciosComponent
   },
   {
     path: '**',
--- src/app/modules/exercises/exercises.module.ts
+++ src/app/modules/exercises/exercises.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ExercisesRoutingModule } from './exercises-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { EjerciciosComponent } from './pages/ejercicios/ejercicios.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    EjerciciosComponent
   ],
   imports: [
     CommonModule,
